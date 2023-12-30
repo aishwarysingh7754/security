f89f4a18d5f12336a3d257a74233a45613863b6a
aishwarysingh7754 <66073060+aishwarysingh7754@users.noreply.github.com>
2023-12-30 12:25:58 +0530

    30 dec

--- src/main/java/com/ezbank/bank/config/ProjectSecurityConfig.java
+++ src/main/java/com/ezbank/bank/config/ProjectSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -29,14 +30,9 @@ public class ProjectSecurityConfig {
                 return http.build();
         }
 
-        // @Bean
-        // public UserDetailsService userDetailsService(DataSource dataSource) {
-        // return new JdbcUserDetailsManager(dataSource);
-        // }
-
         @Bean
         public PasswordEncoder passwordEncoder() {
-                return NoOpPasswordEncoder.getInstance();
+                return new BCryptPasswordEncoder();
         }
 
 }
